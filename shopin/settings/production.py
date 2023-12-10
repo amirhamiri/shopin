4872286494bed6039f1716dc1dabe445ebf0aa75
amir <amirhamiri74@gmail.com>
2023-12-11 01:35:17 +0330

    setup postgresql

--- shopin/settings/production.py
+++ shopin/settings/production.py
@@ -4,31 +4,26 @@ from corsheaders.defaults import default_headers
 
 DEBUG = False
 print("hiiiiiiiiiiiiiiiiiiiiiiii", DEBUG)
-CORS_ALLOW_HEADERS = list(default_headers) + [
-    "same-origin", "client", "Access-Control-Allow-Origin"
-]
 
-CORS_ALLOWED_ORIGINS = ["*"]
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["*"]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'USER': 'root',
-#         'PASSWORD': '1234',
-#         'HOST': 'mysql-service',
-#         # 'PORT': '80',
-#         'NAME': 'mysql_db',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'shopin_user',
+        'PASSWORD': '12345shopin',
+        'HOST': 'db',
+        'PORT': '5432',
+        'NAME': 'shopin_db',
+    }
+}
 
 STATIC_URL = '/public/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
